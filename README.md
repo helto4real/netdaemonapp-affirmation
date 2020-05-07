72d05188f596d54ecdc741deb887eb86a2c0a01b
helto4real <tomas.hellstrom@yahoo.se>
2020-05-07 18:51:59 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# This is the dev setup for NetDaemon
+
+This is a showcase to create a simple integratio in NetDaemon. It gets a affirmation from www.affirmations.dev and creates a sensor called `sensor.affirmation`
+
+## Issues
+
+If you have issues or suggestions of improvements, please [add an issue](https://github.com/net-daemon/netdaemon/issues)
+
+## Discuss the NetDaemon
+
+Please [join the Discord server](https://discord.gg/K3xwfcX) to get support or if you want to contribute and help others.
+
+## Attribution
+
+ICON: Attribution: [chris](https://commons.wikimedia.org/wiki/User:Chrkl) 論
---
+++ apps/affirmation/AffirmationApp.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading.Tasks;
+using JoySoftware.HomeAssistant.NetDaemon.Common;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+// Use unique namespaces for your apps if you going to share with others to avoid
+// conflicting names
+namespace helto4real
+{
+    public class AffirmationApp : NetDaemonApp
+    {
+        public int? Hours { get; set; }
+        public override Task InitializeAsync()
+        {
+            Scheduler.RunEvery(TimeSpan.FromSeconds(10), UpdateSensorData);
+            return Task.CompletedTask;
+        }
+
+        private async Task UpdateSensorData()
+        {
+            try
+            {
+                //{"affirmation":"I know you'll sort it out"}
+                var todaysDictionary = await Http.GetJson<Dictionary<string, string>>("https://www.affirmations.dev/").ConfigureAwait(false);
+                if (todaysDictionary.TryGetValue("affirmation", out var affirmation))
+                {
+                    await SetState("sensor.affirmation", "on", ("text", affirmation), ("attribution", "www.affirmations.dev"));
+                }
+            }
+            catch (Exception e)
+            {
+                Log(LogLevel.Trace, e, "Error getting affirmation");
+                await SetState("sensor.affirmation", "unavailable", ("text", "unavailable"!));
+            }
+        }
+    }
+}
+
